8077e8d04df33c03ad92497171d3efa68d77e249
Majimus <majimusprime@gmail.com>
2017-10-21 22:13:40 -0500

    Update all devices with one cloud call, dynamic update and debug settings.

--- L_MajimusLifx.lua
+++ L_MajimusLifx.lua
@@ -47,8 +47,8 @@ local g_appendPtr
 
 --debug mode
 DEBUG = 0
---update bulbs every 2 mins
-DELAY = 120
+--update bulbs every 3 mins
+DELAY = 180
 
 --we should run the stats updater only once
 STATS_RUNNING = false
@@ -74,6 +74,7 @@ local function log(textParm, level)
 end
 
 local function lifx_ctrl(selector, mode, color, bright, cycles, period)
+    log("Lifx cloud ctrl",1)
 	https.TIMEOUT = 7
 	local resp = {}
 	local payload = ''
@@ -89,9 +90,9 @@ local function lifx_ctrl(selector, mode, color, bright, cycles, period)
 	cycles = cycles or 1  -- if period nil or false then use 1
 	
 	if selector then
-		log("Entry Selector:"..selector)
+		log("Entry Selector:"..selector,2)
 	else
-		log("Entry Selector: nil")
+		log("Entry Selector: nil",2)
 		if mode == "list" then
 			selector = "all"
 		end
@@ -99,7 +100,7 @@ local function lifx_ctrl(selector, mode, color, bright, cycles, period)
 	
 	selector = selector or luup.variable_get(SID, "LightId", luup.device) or ""
 	
-	log("Final Selector:"..selector)
+	log("Final Selector:"..selector,2)
 		
 	if mode == "pulse" then
 		selmethod = "POST"
@@ -145,7 +146,7 @@ local function lifx_ctrl(selector, mode, color, bright, cycles, period)
 	end
 	
 	if(payload:len()> 0) then
-		log("payload"..payload)
+		log("payload"..payload,2)
 	end
 	
 	local result, statuscode, response_body, status = https.request {
@@ -160,14 +161,20 @@ local function lifx_ctrl(selector, mode, color, bright, cycles, period)
 	 }
 	
 	if(resp[1] == nil) then
-	    log("Lifx NULL response") 
+	    log("Lifx NULL response",1) 
 		return
 	end
-	 
-	log("lifx: "..resp[1], 2)
 	
 	local jsondata = json.parse(resp[1])  --resp is a table where resp[1] is string containing json
+	
+	--for the list command we return multidata
+	if(mode == "list") then
+		return jsondata
+	end	
+	
+	--return status for individual light command
 	key, value = pairs(jsondata)
+	
 	if type(value["results"]) == "table" then 
 		jsondata = jsondata.results[1]
 	elseif type(value[1]) == "table" then 
@@ -189,11 +196,6 @@ local function lifx_ctrl(selector, mode, color, bright, cycles, period)
 		end
 	end
 	
-	--for the list command we return multidata
-	if(mode == "list") then
-		return power, brightness
-	end
-	
 	--return stat, power, connected, err, resp[1]
 	if(stat == "ok") then
 		return true
@@ -211,16 +213,14 @@ end
 
 function turnOn(lul_device)
 	log("TurnOn")
-	local sel = luup.variable_get(SID, "LightId", lul_device)
-	
+	local sel = luup.variable_get(SID, "LightId", lul_device)	
 	--handle the scene
 	if(luup.devices[lul_device].device_type == SDID) then
 		log("Play Scene")
 		delay = luup.variable_get(SID, "SceneDelay", lul_device) or "0"
 		lifx_ctrl(sel, "scene", nil, nil, nil, delay)
 		return
-	end
-	
+	end	
 	--lifx_ctrl(selector, mode, color, bright, cycles, period)
 	local stat = lifx_ctrl(sel, "on", nil, nil, nil, nil)	
 	-- do not update if we returned
@@ -234,8 +234,7 @@ function turnOn(lul_device)
 end
 
 function turnOff(lul_device)
-	log("TurnOff") 
-	
+	log("TurnOff")	
 	if (luup.devices[lul_device].device_type == BDID) then
 		log("Turn off light")
 		local sel = luup.variable_get(SID, "LightId", lul_device)	
@@ -247,8 +246,7 @@ function turnOff(lul_device)
 		end
 	elseif (luup.devices[lul_device].device_type == SDID) then
 		log("Turn off a scene, nothing to do")
-	end
-	
+	end	
 	luup.variable_set(SWITCH_SID, "Status", 0, lul_device)
 	luup.variable_set(DIMMER_SID,"LoadLevelStatus", 0, lul_device)
 end
@@ -280,7 +278,6 @@ function setLoadLevelTarget(target,lul_device)
 
 end
 
-
 function setColorRGB(RGBTarget,lul_device)
 	--lifx_ctrl(selector, mode, color, bright, cycles, period)
 	target = 'rgb:'..RGBTarget
@@ -289,27 +286,74 @@ function setColorRGB(RGBTarget,lul_device)
 	lifx_ctrl(sel, 'color', target, nil, nil, nil)
 end
 
---call delay function to update lights 
---we can do things like not update if 
---recently updated manually etc.
 --TODO update color
---The reason for this will become apparent
---when we move to parent child layout
 function updateStats()
 	log("Updating stats")
+	
+	--update the debug level periodically.
+	local dlevel = luup.variable_get(SID, "DEBUG", luup.device) or "0"
+	local id = "INVALID"
+	dlevel = tonumber(dlevel)
+	DEBUG = dlevel	
+	
+	--set status update times
+	local update = luup.variable_get(SID, "DELAY", luup.device) or "60"
+	update = tonumber(update)
+	DELAY = update
+	
+	status_data = lifx_ctrl('all', 'list', nil, nil, nil, nil);
+	
+	--handle error
+	if(status_data == nil) then
+		log("Stats Failure")
+	end
+	
+	brightness_tab = {}
+	power_tab = {}
+	
+	for status_key, status_value in pairs(status_data) do
+	    cstat = 0
+		for key, value in pairs(status_value) do
+			if type(value) == "string" or type(value) == "number" then 
+				log("Lifx Key:"..key.." Value:"..value,2)
+			end
+			if key == "power" then
+				power = value
+				cstat = cstat + 1
+			elseif key == "connected" then
+				connected = value
+			elseif key == "status" then
+				stat = value
+			elseif key == "brightness" then
+				bright = value
+				cstat = cstat + 1
+			elseif key == "id" then
+				id = "id:"..value
+				cstat = cstat + 1
+			elseif key == "error" then
+				err = value
+			end			
+			if(cstat == 3) then
+				cstat = 0;
+				brightness_tab[id] = bright
+				power_tab[id] = power
+				log("Lifx Saving "..id..":"..power..":"..bright,2)
+			end			
+		end
+	end
+	
 	for k, v in pairs(luup.devices) do
 		if (v and v.device_type == BDID) then
-			log("UpdateStats Dev#:"..k)
-			id=luup.variable_get(SID, "LightId", k)
-			power, bright = lifx_ctrl(id, 'list', nil, nil, nil, nil)
-			
+			log("UpdateStats Dev#:"..k,1)
+			id=luup.variable_get(SID, "LightId", k)			
+			bright = brightness_tab[id]
+			power  = power_tab[id]			
 			if(bright == nil or power == nil) then
 				log("Stats Failure")
 				bright = 0
 				power = "Failure"
-			end
-			
-			log('Power:'..power..' Bright:'..bright)
+			end			
+			log('Power:'..power..' Bright:'..bright,2)
 			loadLevel = tonumber(bright)
 			loadLevel = loadLevel * 100
 			if (power == "on") then
@@ -324,8 +368,8 @@ function updateStats()
 	end
 	--call it again after a while
 	math.randomseed(os.time())
-	variance  =  math.random(1,5)
-	log("Stats Variance:"..variance)
+	variance  =  math.random(1,25)
+	log("Stats Variance:"..variance,2)
 	luup.call_delay("updateStats",DELAY+variance)
 end
 
@@ -418,9 +462,18 @@ function bootStrap(lul_device)
 		local dlevel = luup.variable_get(SID, "DEBUG", lul_device) or "0"
 		dlevel = tonumber(dlevel)
 		DEBUG = dlevel
-		if( dlevel == 1) then
+		if( dlevel == 0) then
 			luup.variable_set(SID, "DEBUG", dlevel,lul_device)
 		end
+		
+		--set status update times
+		local update = luup.variable_get(SID, "DELAY", lul_device) or "180"
+		update = tonumber(update)
+		DELAY = update
+		if(update == 180) then
+			luup.variable_set(SID, "DELAY", update,lul_device)
+		end
+		
 		startParent(lul_device)
 	else
 		--startChild(lul_device)
